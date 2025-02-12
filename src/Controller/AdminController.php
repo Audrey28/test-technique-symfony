c687108cb321f13e02f160f158629e6e0f582df1
ALLAIN <allain-audrey@hotmail.fr>
2025-02-12 21:18:00 +0100

    FEAT 8: Liste des appels

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Specialist;
 use App\Form\SpecialistType;
 use App\Repository\SpecialistRepository;
+use App\Repository\AppelRepository;
 use Doctrine\ORM\EntityManagerInterface; 
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -51,6 +52,14 @@ class AdminController extends AbstractController
 
         return $this->render('admin/specialist_form.html.twig', [
             'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/appels', name: 'appels_list')]
+    public function listAppels(AppelRepository $appelRepository): Response
+    {
+        return $this->render('admin/appels.html.twig', [
+            'appels' => $appelRepository->findAll(),
         ]);
     }
 }
