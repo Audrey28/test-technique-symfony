aba2dc36cd558fe41281123880f823eaf1c32bce
ALLAIN <allain-audrey@hotmail.fr>
2025-02-12 13:40:12 +0100

    FEAT: 4 - ajout de la page d'administration

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+
+
+// Définit /admin comme base pour toutes les routes du contrôleur.
+#[Route('/admin', name:'admin_')]
+class AdminController extends AbstractController
+{
+    #[Route('/', name: 'dashboard')]
+    #[IsGranted('ROLE_ADMIN')]
+    public function index(): Response
+    {
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+}
